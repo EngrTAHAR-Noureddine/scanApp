f7b87399062f8943945930eeb7b8dac06f0a6698
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-18 05:38:55 +0100

    "add site model"

---
+++ lib/models/database_models/sites.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+Site siteFromJson(String str) {
+  final jsonData = json.decode(str);
+  return Site.fromMap(jsonData);
+}
+
+String siteToJson(Site data) {
+  final dyn = data.toMap();
+  return json.encode(dyn);
+}
+
+class Site {
+  int id;
+  String nom;
+
+  Site({
+    this.id,
+    this.nom,
+
+  });
+
+  factory Site.fromMap(Map<String, dynamic> json) => new Site(
+    id: json["id"],
+    nom: json["nom"],
+
+  );
+
+  Map<String, dynamic> toMap() => {
+    "id": id,
+    "nom": nom,
+  };
+}
