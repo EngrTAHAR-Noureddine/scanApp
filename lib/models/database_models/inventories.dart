ff4b8be9e54b5605be999b334cbcf1173c3fdeac
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-18 05:37:44 +0100

    "add inventory model"

---
+++ lib/models/database_models/inventories.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+Inventory inventoryFromJson(String str) {
+  final jsonData = json.decode(str);
+  return Inventory.fromMap(jsonData);
+}
+
+String inventoryToJson(Inventory data) {
+  final dyn = data.toMap();
+  return json.encode(dyn);
+}
+
+class Inventory {
+  int id;
+  String openingDate;
+  String closeDate;
+
+
+  Inventory({
+    this.id,
+    this.openingDate,
+    this.closeDate,
+
+  });
+
+  factory Inventory.fromMap(Map<String, dynamic> json) => new Inventory(
+    id: json["id"],
+    openingDate: json["openingDate"],
+    closeDate: json["closeDate"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "id": id,
+    "openingDate": openingDate,
+    "closeDate": closeDate,
+
+  };
+}
