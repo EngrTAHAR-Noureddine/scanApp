d59cef2970b76b50432c943325c8d814b6c3fa35
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-18 05:37:29 +0100

    "add emplacement model"

---
+++ lib/models/database_models/emplacements.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+Emplacement emplacementFromJson(String str) {
+  final jsonData = json.decode(str);
+  return Emplacement.fromMap(jsonData);
+}
+
+String emplacementToJson(Emplacement data) {
+  final dyn = data.toMap();
+  return json.encode(dyn);
+}
+
+class Emplacement {
+  int id;
+  String nom;
+  int entrepotId;
+  String barCodeEmp;
+
+
+  Emplacement({
+    this.id,
+    this.nom,
+    this.entrepotId,
+    this.barCodeEmp
+
+  });
+
+  factory Emplacement.fromMap(Map<String, dynamic> json) => new Emplacement(
+    id: json["id"],
+    nom: json["nom"],
+    entrepotId: json["entrepotId"],
+    barCodeEmp: json["barcodeemp"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "id": id,
+    "nom": nom,
+    "entrepotId": entrepotId,
+    "barcodeemp": barCodeEmp,
+  };
+}
