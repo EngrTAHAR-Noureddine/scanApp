8a09e15444d538e5e8cd67650ce015d402b5a5f8
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-21 23:45:27 +0100

    "add export fichier xlsx"

--- lib/view_models/providers/exports_list.dart
+++ lib/view_models/providers/exports_list.dart
@@ -1,10 +1,135 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:scanapp/data/database.dart';
+import 'package:scanapp/models/database_models/inventories.dart';
+import 'package:scanapp/models/database_models/inventory_lines.dart';
+import 'package:syncfusion_flutter_xlsio/xlsio.dart';
 
 class ExportProvider extends ChangeNotifier{
   static ExportProvider? _instance;
   ExportProvider._();
   factory ExportProvider() => _instance ??=ExportProvider._();
 
+  Future<bool> _requestPermission(Permission per) async{
+
+    if(await per.isGranted){
+      return true;
+    }else {
+      var result = await per.request();
+      if(result == PermissionStatus.granted){ return true; }else {return false;}
+    }
+
+  }
+
+  Future<void> saveInFile(List<InventoryLine> inventoryLines,String fileName)async{ // notnull
+    /* variables  */
+    fileName = fileName + '.xlsx';
+    String newPath ="";
+    File file;
+
+    /* instructors  */
+
+    Directory? directory;
+    try{
+      /// get the directory of FecomIt folder  *********************************************************
+      if(Platform.isAndroid){  /* Android  */
+        if(await _requestPermission(Permission.storage)){
+          directory = await getExternalStorageDirectory();
+          if(directory!=null){
+            print(directory.path);
+
+            List<String> folders = directory.path.split("/");
+            for(int x=1; x<folders.length;x++){
+              String folder = folders[x];
+              if(folder != "Android"){
+                newPath += "/"+folder;
+              }else break;
+            }
+            newPath = newPath+"/FecomIt";
+            directory = Directory(newPath);
+
+
+                newPath = newPath +"/"+fileName;
+                file = File(newPath)..createSync(recursive: true);
+
+                if((await file.exists())==true){
+
+                  /* write here */
+
+                  try {
+                    final Workbook workbook = Workbook();
+                    final Worksheet sheet = workbook.worksheets[0];
+
+                        int row =0;
+                        final List<Object> firstLine = ["id", "Id_inventory", "Id_Produit", "Id_Emplacement","Id_productLot","quantity","quantitySystem","difference","quality"];
+                        sheet.importList(firstLine, row, 1, false);
+
+                        inventoryLines.forEach((element) {
+                          row++;
+                          final List<Object> lineRow = [element.id??"-----",
+                                                          element.inventoryId??"-----",
+                                                          element.productId??"-----",
+                                                          element.emplacementId??"-----",
+                                                          element.productLotId??"-----",
+                                                          element.quantity??"-----",
+                                                          element.quantitySystem??"-----",
+                                                          element.difference??"-----",
+                                                          element.quality??"-----"];
+                          sheet.importList(lineRow, row, 1, false);
+                        });
+                        final List<int> bytes = workbook.saveAsStream();
+                        await file.writeAsBytes(bytes, flush: true);
+                        workbook.dispose();
+                        OpenFile.open(file.path);
+
+                      } catch (e) {
+                        print(e);
+                      }
+
+                }else {
+                 // throw "Android11";
+                }
+
+
+          }
+
+
+        }//else throw "permission";
+      }else print("the platform is not ios");
+    }catch(e){ print("catch error : "+e.toString());}
+
+  }
+
+  Future<List<Inventory>> getAllInventory()async{
+    List<Inventory>? list = await DBProvider.db.getAllInventories();
+    List<Inventory> anotherList = <Inventory>[];
+
+    if(list != null){
+
+      list.forEach((element) {
+        if(element.status != "begin"){
+          anotherList.add(element);
+        }
+      });
+    }else anotherList = [];
+    return (anotherList.isNotEmpty)?anotherList:[];
+  }
+
+  Future<void> getInventoryLines(int? id)async{
+
+    List<InventoryLine>? list = await DBProvider.db.getAllInventoryLines(id);
+    String name = 'inventory'+id.toString()+"_"+DateTime.now().toString();
+
+
+    if(list!=null){
+      await saveInFile(list ,name );
+    }
+
+  }
 
 
 }
--- lib/views/exports_list.dart
+++ lib/views/exports_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:scanapp/models/database_models/inventories.dart';
 import 'package:scanapp/models/variables_define/colors.dart';
 import 'package:scanapp/view_models/providers/exports_list.dart';
 
@@ -12,62 +13,96 @@ class Export extends StatelessWidget {
 
           return Scaffold(
             backgroundColor: ColorsOf().backGround(),
-            body: Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.only(top: 0, left: 10, right: 10, bottom: 10),
-              child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  itemCount: 10,
-                  padding: EdgeInsets.all(5),
-                  itemBuilder: (BuildContext context, int index){
+            body: FutureBuilder(
+              future: value.getAllInventory(),
+              builder: (context, snapshot) {
 
-                    return Container(
-                        margin: EdgeInsets.all(5),
-                        child:ListTile(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(10)),
-                              side: BorderSide(color:ColorsOf().primaryBackGround() ,width: 1,style: BorderStyle.solid)
-                          ),
-                          leading: Icon(Icons.inventory, color: ColorsOf().containerThings(),),
-                          trailing:Container(
-                            width: 100,
-                            height: 50,
-                            color: Colors.transparent,
-                            alignment: Alignment.center,
-                            child:MaterialButton(
-                              padding: EdgeInsets.zero,
-                              height: 50,
-                              minWidth: 100,
-                              color: ColorsOf().containerThings(),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(100)),
-                                  side: BorderSide(color:ColorsOf().containerThings() ,width: 1,style: BorderStyle.solid)
-                              ),
-                              child: Text("Export",style: TextStyle(fontSize: 16, color: ColorsOf().primaryBackGround()),),
+                    if (snapshot.connectionState == ConnectionState.done && snapshot.hasData && snapshot.data!=null) {
+                      List<Inventory> list = snapshot.data as List<Inventory>;
+                      if(list.isNotEmpty){
+                        return Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                          padding: EdgeInsets.only(top: 0, left: 10, right: 10, bottom: 10),
+                          child: ListView.builder(
+                              scrollDirection: Axis.vertical,
+                              itemCount: list.length,
+                              padding: EdgeInsets.all(5),
+                              itemBuilder: (BuildContext context, int index){
 
-                              onPressed: (){},
+                                return Container(
+                                    margin: EdgeInsets.all(5),
+                                    child:ListTile(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                                          side: BorderSide(color:ColorsOf().primaryBackGround() ,width: 1,style: BorderStyle.solid)
+                                      ),
+                                      leading: Icon(Icons.inventory, color: ColorsOf().containerThings(),),
+                                      trailing:Container(
+                                        width: 100,
+                                        height: 50,
+                                        color: Colors.transparent,
+                                        alignment: Alignment.center,
+                                        child:MaterialButton(
+                                          padding: EdgeInsets.zero,
+                                          height: 50,
+                                          minWidth: 100,
+                                          color: ColorsOf().containerThings(),
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.all(Radius.circular(100)),
+                                              side: BorderSide(color:ColorsOf().containerThings() ,width: 1,style: BorderStyle.solid)
+                                          ),
+                                          child: Text("Export",style: TextStyle(fontSize: 16, color: ColorsOf().primaryBackGround()),),
 
-                            ),),
-                          title: RichText(
-                              text: TextSpan(children: [
+                                          onPressed:()=>value.getInventoryLines(list[index].id),
 
-                                TextSpan(text:"Inventory : ",
-                                  style: TextStyle(color : ColorsOf().profilField() ,fontSize: 14 ,fontWeight: FontWeight.bold),
-                                ),
-                                TextSpan(text:"100000",
-                                  style: TextStyle(color : ColorsOf().backGround() ,fontSize: 14 ,fontWeight: FontWeight.bold),
-                                ),
+                                        ),),
+                                      title: RichText(
+                                          text: TextSpan(children: [
 
-                              ])
-                          ),
+                                            TextSpan(text:"Inventory : ",
+                                              style: TextStyle(color : ColorsOf().profilField() ,fontSize: 14 ,fontWeight: FontWeight.bold),
+                                            ),
+                                            TextSpan(text:(list[index].id!=null)?list[index].id.toString():"------",
+                                              style: TextStyle(color : ColorsOf().backGround() ,fontSize: 14 ,fontWeight: FontWeight.bold),
+                                            ),
 
-                          tileColor:ColorsOf().primaryBackGround(),
-                          onTap:null,
-                        )
-                    );
-                  }),
+                                          ])
+                                      ),
+
+                                      tileColor:ColorsOf().primaryBackGround(),
+                                      onTap:null,
+                                    )
+                                );
+                              }),
+                        );
+                      }else{
+                        return Container(
+                          color:ColorsOf().backGround(),
+                          alignment: Alignment.center,
+                          child: Text("Vide" , style: TextStyle(color: ColorsOf().primaryBackGround(),fontSize: 20),),
+                        );
+                      }
+
+                    }else if(snapshot.connectionState == ConnectionState.waiting){
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height,
+                          child: Text("Rechercher..." , style: TextStyle(color: ColorsOf().primaryBackGround(),fontSize: 20),),
+
+                          color: ColorsOf().backGround());
+                    }else {
+
+                      return Container(
+                        color:ColorsOf().backGround(),
+                        alignment: Alignment.center,
+                        child: Text("Vide" , style: TextStyle(color: ColorsOf().primaryBackGround(),fontSize: 20),),
+                      );
+                    }
+
+              }
             ),
+
           );
 
         });
