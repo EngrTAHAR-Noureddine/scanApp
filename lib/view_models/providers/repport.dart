67d8c6c4d8fc8ac0904312216cd907ccfdedb790
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-21 05:11:37 +0100

    "finish report view"

--- lib/view_models/providers/repport.dart
+++ lib/view_models/providers/repport.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:scanapp/data/database.dart';
+import 'package:scanapp/models/database_models/counter_stocks_in_emplacement.dart';
+import 'package:scanapp/models/database_models/inventories.dart';
+import 'package:scanapp/models/database_models/inventory_lines.dart';
 
 class RepportProvider extends ChangeNotifier{
 
@@ -6,8 +10,76 @@ class RepportProvider extends ChangeNotifier{
   RepportProvider._();
   factory RepportProvider() => _instance ??=RepportProvider._();
 
+  int total = 0;
+  int scan =0;
+  List<StocksCounter>? listLinesEmp;
+
+  Future<List<EmplacementInfo>> getAllStatistics()async{
+     total = 0;
+     scan =0;
+
+
+    List<StocksCounter>? listStocks = await DBProvider.db.saveStocksOfEmplacement();
+    List<StocksCounter>? listLinesEmp = await DBProvider.db.getEachEmplacementStocks();
+    List<EmplacementInfo> emplacements = <EmplacementInfo>[];
 
 
 
+      if(listStocks.isNotEmpty){
+
+
+
+        for(int i =0 ; i< listStocks.length ;i++){
+          StocksCounter stock = listStocks[i];
+          total = (stock.number!=null)?(total + stock.number!):(total +0);
+          String? name = (await DBProvider.db.getEmplacement(stock.emplacementID))?.nom ?? null;
+            EmplacementInfo empInfo = new EmplacementInfo(
+                nom: name,
+                id: stock.emplacementID,
+                total: stock.number??0,
+                scan: 0
+            );
+            emplacements.add(empInfo);
+        }
+      }
+
+      if(listLinesEmp.isNotEmpty){
+
+
+
+        listLinesEmp.forEach((line) {
+
+          scan  = (line.number!=null)?(scan + line.number!):(scan +0);
+
+          emplacements.forEach((stock) {
+
+            if(stock.id == line.emplacementID){
+              stock.scan = line.number??0;
+            }
+
+          });
+
+
+        });
+      }
+
+
+
+
+    return (emplacements.isNotEmpty)?emplacements:[];
+
+ }
+
+
+}
+
+class EmplacementInfo{
+
+  int? id;
+  String? nom;
+  int? total;
+  int? scan;
+
+  EmplacementInfo({required this.id,required this.scan,required this.total , required this.nom});
 
 }
--- lib/views/repport.dart
+++ lib/views/repport.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:scanapp/models/database_models/counter_stocks_in_emplacement.dart';
 import 'package:scanapp/models/variables_define/colors.dart';
 import 'package:scanapp/view_models/providers/repport.dart';
 
@@ -12,162 +13,184 @@ class Repport extends StatelessWidget {
 
           return Scaffold(
             backgroundColor: ColorsOf().backGround(),
-            body: Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
+            body: FutureBuilder(
+              future: value.getAllStatistics(),
+              builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done && snapshot.hasData && snapshot.data != null) {
+                      List<EmplacementInfo> list = snapshot.data as List<EmplacementInfo>;
+                      return Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
 
-              child: Column(
-                children: [
-                  Container(
-                    height: 100,
-                    color: ColorsOf().backGround(),
-                    child:Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          alignment: Alignment.topLeft,
-                          padding: EdgeInsets.only(top:5,bottom: 5),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                flex: 1,
-                                child: Padding(
-                                  padding:EdgeInsets.only(left: 5,right: 5),
-                                  child: Text("Company>Service>Bureau>Emplacement" , style: TextStyle(color : ColorsOf().importField() ,fontSize: 10 ,fontWeight: FontWeight.normal , fontStyle:FontStyle.italic),),
-                                ),
-                              ),
-
-                            ],
-                          ),
-                        ),
-                        Container(
-                          alignment: Alignment.bottomLeft,
-
-                          height: 50,
-                          child: Row(
-                            children: [
-                              Expanded(
-                                flex: 1,
-                                child: Padding(
-                                    padding:EdgeInsets.only(left: 10),
-                                    child: RichText(
-                                        text: TextSpan(children: [
-
-                                          TextSpan(text:"Total : ",
-                                            style: TextStyle(color : ColorsOf().borderContainer() ,fontSize: 14 ,fontWeight: FontWeight.bold),
-                                          ),
-                                          TextSpan(text:"100",
-                                            style: TextStyle(color : ColorsOf().primaryForGround() ,fontSize: 14 ,fontWeight: FontWeight.bold),
-                                          ),
-
-                                        ])
-                                    )
-
-                                ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Container(
-                                    alignment: Alignment.centerRight,
-                                    padding: EdgeInsets.only(right: 10),
-                                    child: RichText(
-                                        text: TextSpan(children: [
-
-                                          TextSpan(text:"Scan : ",
-                                            style: TextStyle(color : ColorsOf().borderContainer() ,fontSize: 14 ,fontWeight: FontWeight.bold),
-                                          ),
-                                          TextSpan(text:"23",
-                                            style: TextStyle(color : ColorsOf().primaryForGround() ,fontSize: 14 ,fontWeight: FontWeight.bold),
-                                          ),
-
-                                        ])
-                                    )
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      padding: EdgeInsets.only(top: 0, left: 10, right: 10, bottom: 10),
-                      child: ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          itemCount: 10,
-                          padding: EdgeInsets.all(5),
-                          itemBuilder: (BuildContext context, int index){
-
-                            return Container(
-                                margin: EdgeInsets.all(5),
-                                child:ListTile(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                                      side: BorderSide(color:ColorsOf().primaryBackGround() ,width: 1,style: BorderStyle.solid)
-                                  ),
-                                  leading: Icon(Icons.inventory, color: ColorsOf().containerThings(),),
-                                  title:  RichText(
-                                      text: TextSpan(children: [
-
-                                        TextSpan(text:"Emplacement : ",
-                                          style: TextStyle(color : ColorsOf().profilField() ,fontSize: 16 ,fontWeight: FontWeight.bold),
-                                        ),
-                                        TextSpan(text:"100",
-                                          style: TextStyle(color : ColorsOf().backGround() ,fontSize: 16 ,fontWeight: FontWeight.bold),
-                                        ),
-
-                                      ])
-                                  ),
-                                  subtitle:Container(
-                                    alignment: Alignment.centerLeft,
-                                    margin: EdgeInsets.only(top: 5),
-                                    child:Column(
-                                      mainAxisAlignment: MainAxisAlignment.start,
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 100,
+                              color: ColorsOf().backGround(),
+                              child:Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    alignment: Alignment.topLeft,
+                                    padding: EdgeInsets.only(top:5,bottom: 5),
+                                    child: Row(
                                       children: [
-                                        Container(
-                                          alignment: Alignment.topLeft,
-                                          child: RichText(
-                                              text: TextSpan(children: [
-
-                                                TextSpan(text:"Total : ",
-                                                  style: TextStyle(color : ColorsOf().profilField() ,fontSize: 14 ,fontWeight: FontWeight.bold),
-                                                ),
-                                                TextSpan(text:"100",
-                                                  style: TextStyle(color : ColorsOf().backGround() ,fontSize: 14 ,fontWeight: FontWeight.normal),
-                                                ),
-
-                                              ])
+                                        Expanded(
+                                          flex: 1,
+                                          child: Padding(
+                                            padding:EdgeInsets.only(left: 5,right: 5),
+                                            child: Text("" , style: TextStyle(color : ColorsOf().importField() ,fontSize: 10 ,fontWeight: FontWeight.normal , fontStyle:FontStyle.italic),),
                                           ),
                                         ),
-                                        SizedBox(height: 5,),
-                                        Container(
-                                          alignment: Alignment.topLeft,
-                                            child: RichText(
+
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    alignment: Alignment.bottomLeft,
+
+                                    height: 50,
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          flex: 1,
+                                          child: Padding(
+                                              padding:EdgeInsets.only(left: 10),
+                                              child: RichText(
+                                                  text: TextSpan(children: [
+
+                                                    TextSpan(text:"Total : ",
+                                                      style: TextStyle(color : ColorsOf().borderContainer() ,fontSize: 14 ,fontWeight: FontWeight.bold),
+                                                    ),
+                                                    TextSpan(text:value.total.toString(),
+                                                      style: TextStyle(color : ColorsOf().primaryForGround() ,fontSize: 14 ,fontWeight: FontWeight.bold),
+                                                    ),
+
+                                                  ])
+                                              )
+
+                                          ),
+                                        ),
+                                        Expanded(
+                                          flex: 1,
+                                          child: Container(
+                                              alignment: Alignment.centerRight,
+                                              padding: EdgeInsets.only(right: 10),
+                                              child: RichText(
+                                                  text: TextSpan(children: [
+
+                                                    TextSpan(text:"Scan : ",
+                                                      style: TextStyle(color : ColorsOf().borderContainer() ,fontSize: 14 ,fontWeight: FontWeight.bold),
+                                                    ),
+                                                    TextSpan(text:value.scan.toString(),
+                                                      style: TextStyle(color : ColorsOf().primaryForGround() ,fontSize: 14 ,fontWeight: FontWeight.bold),
+                                                    ),
+
+                                                  ])
+                                              )
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                padding: EdgeInsets.only(top: 0, left: 10, right: 10, bottom: 10),
+                                child: ListView.builder(
+                                    scrollDirection: Axis.vertical,
+                                    itemCount: list.length,
+                                    padding: EdgeInsets.all(5),
+                                    itemBuilder: (BuildContext context, int index){
+
+                                      return Container(
+                                          margin: EdgeInsets.all(5),
+                                          child:ListTile(
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                                                side: BorderSide(color:ColorsOf().primaryBackGround() ,width: 1,style: BorderStyle.solid)
+                                            ),
+                                            leading: Icon(Icons.inventory, color: ColorsOf().containerThings(),),
+                                            title:  RichText(
                                                 text: TextSpan(children: [
 
-                                                  TextSpan(text:"Scan : ",
-                                                    style: TextStyle(color : ColorsOf().profilField() ,fontSize: 14 ,fontWeight: FontWeight.bold),
+                                                  TextSpan(text:"Emplacement : ",
+                                                    style: TextStyle(color : ColorsOf().profilField() ,fontSize: 16 ,fontWeight: FontWeight.bold),
                                                   ),
-                                                  TextSpan(text:"23",
-                                                    style: TextStyle(color : ColorsOf().backGround() ,fontSize: 14 ,fontWeight: FontWeight.normal),
+                                                  TextSpan(text:(list[index].nom!=null)?list[index].nom:(list[index].id!=null)?list[index].id.toString():"------",
+                                                    style: TextStyle(color : ColorsOf().backGround() ,fontSize: 16 ,fontWeight: FontWeight.bold),
                                                   ),
 
                                                 ])
-                                            )
-                                        ),
-                                      ],
-                                    ),),
-                                  tileColor:ColorsOf().primaryBackGround(),
-                                  onTap:null,
-                                )
-                            );
-                          }),
-                    ),
-                  ),
-                ],
-              ),
+                                            ),
+                                            subtitle:Container(
+                                              alignment: Alignment.centerLeft,
+                                              margin: EdgeInsets.only(top: 5),
+                                              child:Column(
+                                                mainAxisAlignment: MainAxisAlignment.start,
+                                                children: [
+                                                  Container(
+                                                    alignment: Alignment.topLeft,
+                                                    child: RichText(
+                                                        text: TextSpan(children: [
+
+                                                          TextSpan(text:"Total : ",
+                                                            style: TextStyle(color : ColorsOf().profilField() ,fontSize: 14 ,fontWeight: FontWeight.bold),
+                                                          ),
+                                                          TextSpan(text:(list[index].total!=null)? list[index].total.toString():"------",
+                                                            style: TextStyle(color : ColorsOf().backGround() ,fontSize: 14 ,fontWeight: FontWeight.normal),
+                                                          ),
+
+                                                        ])
+                                                    ),
+                                                  ),
+                                                  SizedBox(height: 5,),
+                                                  Container(
+                                                      alignment: Alignment.topLeft,
+                                                      child: RichText(
+                                                          text: TextSpan(children: [
+
+                                                            TextSpan(text:"Scan : ",
+                                                              style: TextStyle(color : ColorsOf().profilField() ,fontSize: 14 ,fontWeight: FontWeight.bold),
+                                                            ),
+                                                            TextSpan(text:(list[index].scan!=null)?(list[index].total != null)?(list[index].scan! > list[index].total!)? list[index].scan.toString()+" avec répétition":list[index].scan.toString():list[index].scan.toString():"------",
+                                                              style: TextStyle(color : ColorsOf().backGround() ,fontSize: 14 ,fontWeight: FontWeight.normal),
+                                                            ),
+
+                                                          ])
+                                                      )
+                                                  ),
+                                                ],
+                                              ),),
+                                            tileColor:ColorsOf().primaryBackGround(),
+                                            onTap:null,
+                                          )
+                                      );
+                                    }),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }else if(snapshot.connectionState == ConnectionState.waiting){
+                                return Container(
+                                  color:ColorsOf().backGround(),
+                                  alignment: Alignment.center,
+                                  child: Text("Recherche..." , style: TextStyle(color: ColorsOf().primaryBackGround(),fontSize: 20),),
+                                );
+                    }else{
+                              return Container(
+                                color:ColorsOf().backGround(),
+                                alignment: Alignment.center,
+                                child: Text("Vide" , style: TextStyle(color: ColorsOf().primaryBackGround(),fontSize: 20),),
+                              );
+                    }
+
+
+              }
             ),
           );
 
